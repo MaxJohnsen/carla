e1110b6f89c89cffb4822c54020b10941d6773b6
Hege Haavaldsen <hegehaav@stud.ntnu.no>
2019-04-25 14:43:19 +0200

    Create separate file for all enums

---
+++ PythonAPI/custom/enums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class RoadOption(Enum):
+    """
+    RoadOption represents the possible topological configurations when moving from a segment of lane to other.
+    """
+    VOID = -1
+    LEFT = 1
+    RIGHT = 2
+    STRAIGHT = 3
+    LANEFOLLOW = 4
+    CHANGELANELEFT = 5
+    CHANGELANERIGHT = 6
+
+class ControlType(Enum):
+    MANUAL = 0
+    CLIENT_AP = 1
+    SERVER_AP = 2
+    DRIVE_MODEL = 3
+
+class Enviornment(Enum):
+    HIGHWAY = 0
+
+    
